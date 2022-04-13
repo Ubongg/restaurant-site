b3013a6c80ea950fc3f752254c6772a961372584
Ubongg <ubongeffiong5@gmail.com>
2022-04-13 12:34:41 +0100

    added data.js file to hold data

---
+++ src/data.js
@@ -0,0 +1,207 @@
+import React from "react";
+
+export const soups = [
+  {
+    id: 1,
+    price: 15,
+    name: "SPINACH SOUP",
+    image: "/images/menu-item-04.png",
+    ingredients:
+      "Milk, eggs, spinach, garlic, onion, potato, chicken stock, lemon",
+  },
+  {
+    id: 2,
+    price: 12,
+    name: "TOMATO SOUP",
+    image: "/images/menu-item-05.png",
+    ingredients:
+      "Tomatos, onion, carrot, celery, olive oil, tomato puree, sugar, vegetable soup",
+  },
+  {
+    id: 3,
+    price: 17,
+    name: "MUSHROOM SOUP",
+    image: "/images/menu-item-06.png",
+    ingredients:
+      "Mushrooms, olive oil, garlic, onion, sea salt, black pepper, persley, cheese",
+  },
+];
+
+export const salads = [
+  {
+    id: 1,
+    price: 15,
+    name: "CRACKING COBB SALAD",
+    image: "/images/menu-item-07.png",
+    ingredients:
+      "Chicken thighs, pepper, olive oil, salt, pancetta, avocado, tomato, cheese, buttermilk",
+  },
+  {
+    id: 2,
+    price: 12,
+    name: "WALDORF SALAD",
+    image: "/images/menu-item-08.png",
+    ingredients:
+      "Grapes, lemon, olive oil, sea salt, black pepper, walnuts, yoghurt, celery",
+  },
+  {
+    id: 3,
+    price: 17,
+    name: "SPRING QUICHE",
+    image: "/images/menu-item-09.png",
+    ingredients: "Asparagus, spinach, onion, milk, cheese, black pepper",
+  },
+];
+
+export const main = [
+  {
+    id: 1,
+    price: 25,
+    name: "GRILLED CHICKEN KEBABS",
+    image: "/images/menu-item-10.png",
+    ingredients:
+      "Skinless chicken breast, black pepper, red pepper, passata, ginger, garlic",
+  },
+  {
+    id: 2,
+    price: 27,
+    name: "BBQ BEEF SHORT RIBS",
+    image: "/images/menu-item-11.png",
+    ingredients:
+      "Short ribs, olive oil, caraway seeds, yoghurt, carrots, onions, white cabbage",
+  },
+  {
+    id: 3,
+    price: 21,
+    name: "SOUTHERN FIRED CHICKEN",
+    image: "/images/menu-item-12.png",
+    ingredients: "Chicken thighs, chicken drumsticks, potatoes, ayenne pepper",
+  },
+  {
+    id: 4,
+    price: 24,
+    name: "SICILIAN-STYLE TUNA CARPACCIO",
+    image: "/images/menu-item-13.png",
+    ingredients:
+      "Tuna steak, rose vine, red chilli, lemon, fresh basil, fresh dill, garlic, olive oil",
+  },
+  {
+    id: 5,
+    price: 22,
+    name: "TURKEY TONNATO",
+    image: "/images/menu-item-14.png",
+    ingredients:
+      "Turkey breast, red chilli, olive oil, tinned tuna, lemon, black pepper, red wine vinegar",
+  },
+  {
+    id: 6,
+    price: 32,
+    name: "NEXT LEVEL STEAK",
+    image: "/images/menu-item-15.png",
+    ingredients:
+      "Flank skirt steak, onion, butter, olive oil, red wine vinegar",
+  },
+];
+
+export const deserts = [
+  {
+    id: 1,
+    price: 10,
+    name: "CHOCOLATE & ORANGE PLATE",
+    image: "/images/menu-item-16.png",
+    ingredients:
+      "Dark melted chocolate, juicy orange, sugar, lemon juice, vanilla sticks",
+  },
+  {
+    id: 2,
+    price: 11,
+    name: "JAMAICAN STRAWBERRY CAKE",
+    image: "/images/menu-item-17.png",
+    ingredients:
+      "Strawberry, golden syrup, brown sugar, cream cheese, cinnamon",
+  },
+  {
+    id: 3,
+    price: 12,
+    name: "ILE FLOTTANTE",
+    image: "/images/menu-item-18.png",
+    ingredients:
+      "Vanilla pod, rhubard, golden caster sugar, orange, milk, double cream",
+  },
+  {
+    id: 4,
+    price: 12,
+    name: "PANNA COTTA",
+    image: "/images/menu-item-19.png",
+    ingredients:
+      "Milk, powdered gelatin, whipping cream, honey, sugar, salt, fresh berries",
+  },
+  {
+    id: 5,
+    price: 10,
+    name: "TASTY TIRAMISU CAKE",
+    image: "/images/menu-item-20.png",
+    ingredients:
+      "Egg yolks, sugar, milk, mascarpone cheese, coffee, brandy, cocoa powder, chocolate",
+  },
+  {
+    id: 6,
+    price: 11,
+    name: "TANGERINE & VANILLA CUP",
+    image: "/images/menu-item-21.png",
+    ingredients: "Tangerine, vanilla sticks, milk, brown sugar, double cream",
+  },
+];
+
+export const lunch = [
+  {
+    id: 1,
+    price: 27,
+    name: "SPINACH SOUP, ROAST PORK",
+    image: "/images/menu-item-04.png",
+    ingredients:
+      "Milk, eggs, spinach, garlic, onion, potato, chicken stock, lemon",
+  },
+  {
+    id: 2,
+    price: 32,
+    name: "TOMATO SOUP, FRIED CHICKEN",
+    image: "/images/menu-item-05.png",
+    ingredients:
+      "Tomatos, onion, carrot, celery, olive oil, tomato puree, sugar, vegetable soup",
+  },
+  {
+    id: 3,
+    price: 29,
+    name: "MUSHROOM SOUP, BEEF STEAK",
+    image: "/images/menu-item-06.png",
+    ingredients:
+      "Mushrooms, olive oil, garlic, onion, sea salt, black pepper, persley, cheese",
+  },
+];
+
+export const dinner = [
+  {
+    id: 1,
+    price: 30,
+    name: "ROYAL CHICKEN SALAD",
+    image: "/images/menu-item-07.png",
+    ingredients:
+      "Chicken thighs, pepper, olive oil, salt, pancetta, avocado, tomato, cheese, buttermilk",
+  },
+  {
+    id: 2,
+    price: 32,
+    name: "ROYAL BEEF STEAK",
+    image: "/images/menu-item-08.png",
+    ingredients:
+      "Grapes, lemon, olive oil, sea salt, black pepper, walnuts, yoghurt, celery",
+  },
+  {
+    id: 3,
+    price: 28,
+    name: "CHICKEN MUSHROOM SAUCE",
+    image: "/images/menu-item-09.png",
+    ingredients: "Asparagus, spinach, onion, milk, cheese, black pepper",
+  },
+];
